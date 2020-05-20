30f5626ef9ad7bdf99b1a5545ea4b2a1b623a4db
Wilson Neto <wilsonpsn@hotmail.com>
2020-05-20 15:21:55 -0300

    Finished Refactoring player

--- src/characters/Player.ts
+++ src/characters/Player.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
-
+import Bullets from '../skills/Bullets'
 export default class Player extends Phaser.Physics.Arcade.Sprite {
-  
+  private doublePressEligibility = {}
   constructor (
     scene: Phaser.Scene,
     x: number,
@@ -10,7 +10,91 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     frame?: string | number
   ) {
     super(scene, x, y, texture, frame)
-    
+    scene.physics.add.existing(this)
+    scene.sys.displayList.add(this)
+    scene.sys.updateList.add(this)
+    this.setBounce(0.4)
+    this.setCollideWorldBounds(true)
+
+    let angle = 0
+    scene.input.on('pointermove', pointer => {
+      if (!this) return
+      const playerVector = new Phaser.Math.Vector2(this.x, this.y)
+      angle = Phaser.Math.Angle.BetweenPoints(playerVector, pointer)
+    })
+    const bullets = new Bullets(scene)
+
+    scene.input.on('pointerdown', pointer => {
+      if (!this) return
+      console.log('Game -> create -> angle', angle)
+      bullets.fireBullet(this?.body.x, this?.body.y, angle)
+    })
   }
 
+  checkDoubleEligibility (
+    key: Phaser.Input.Keyboard.Key,
+    eligibilityState: Object,
+    time: number
+  ) {
+    //Variable declarations
+    const { keyCode } = key
+    const lastTime = eligibilityState[keyCode]?.lastTime ?? 0
+    const currentTime = time
+    const isJustPressed = Phaser.Input.Keyboard.JustDown(key)
+    const deltaTime = currentTime - lastTime
+    let canDouble = eligibilityState[keyCode]?.canDouble ?? false
+    //Logic
+    isJustPressed && canDouble && deltaTime < 200
+      ? (canDouble = true)
+      : (canDouble = false)
+    if (canDouble)
+      console.table({ hayai: '早い', deltaTime, currentTime, lastTime })
+    //State update
+    eligibilityState[keyCode] = {
+      canDouble: !canDouble, // Can't double if doubled
+      lastTime: currentTime
+    }
+    // Return
+    return canDouble
+  }
+
+  update (t: number, dt: number, cursors) {
+    console.log('oiiii')
+    // Esquerda
+    const sideDash = 8600
+    const sideRun = 160
+    if (!cursors?.right?.isDown && cursors?.left?.isDown) {
+      if (
+        this.checkDoubleEligibility(
+          cursors.left as Phaser.Input.Keyboard.Key,
+          this.doublePressEligibility,
+          t
+        )
+      ) {
+        this.setVelocityX(-sideDash)
+      } else {
+        this.setVelocityX(-sideRun)
+      }
+      this.anims.play('left', true)
+    } else if (cursors?.right?.isDown && !cursors?.left?.isDown) {
+      if (
+        this.checkDoubleEligibility(
+          cursors.right as Phaser.Input.Keyboard.Key,
+          this.doublePressEligibility,
+          t
+        )
+      ) {
+        this.setVelocityX(sideDash)
+      } else {
+        this.setVelocityX(sideRun)
+      }
+      this.anims.play('right', true)
+    } else {
+      this.setVelocityX(0)
+      this.anims.play('turn')
+    }
+    if (cursors?.up?.isDown && this.body.touching.down) {
+      this.setVelocityY(-330)
+    }
+  }
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,4 @@
 import Phaser, { Physics, Time } from 'phaser'
-import Bullets from '../skills/Bullets'
 import Preloader from './Preloader'
 import { createDudeAnims } from '~/anims/dude/dudeAnims'
 import Player from '~/characters/Player'
@@ -7,13 +6,6 @@ export default class Game extends Phaser.Scene {
   private platforms?: Phaser.Physics.Arcade.StaticGroup
   private player!: Physics.Arcade.Sprite
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-  private stars?: Phaser.Physics.Arcade.Group
-  private gameOver = false
-  private score = 0
-  private scoreText?: Phaser.GameObjects.Text
-  private bombs?: Phaser.Physics.Arcade.Group
-  private doublePressCounter = 0
-  private doublePressEligibility = {}
 
   constructor () {
     super('game')
@@ -39,7 +31,7 @@ export default class Game extends Phaser.Scene {
     
      
     
-    this.player = this.physics.add.existing(new Player(this, 20, 20, 'dude')) as Player
+    this.player = (new Player(this, 20, 20, 'dude'))
     // this.player = this.physics.add.sprite(20, 20, 'dude')
     
     this.platforms.create(50, 250, 'ground')
@@ -47,8 +39,6 @@ export default class Game extends Phaser.Scene {
     this.platforms.create(600, 400, 'ground')
     // this.player = this.physics.add.sprite(20, 20, 'dude')
     console.log('Game -> create -> this.player', this.player)
-    this.player.setBounce(0.2)
-    this.player.setCollideWorldBounds(true)
 
     camera.startFollow(this.player, true)
 
@@ -62,86 +52,11 @@ export default class Game extends Phaser.Scene {
       space: Phaser.Input.Keyboard.KeyCodes.SPACE
     })
 
-    let angle = 0
-    this.input.on('pointermove', pointer => {
-      if (!this.player) return
-      const playerVector = new Phaser.Math.Vector2(
-        this.player?.x,
-        this.player?.y
-      )
-      angle = Phaser.Math.Angle.BetweenPoints(playerVector, pointer)
-    })
-    const bullets = new Bullets(this)
-
-    this.input.on('pointerdown', pointer => {
-      if (!this.player) return
-      console.log('Game -> create -> angle', angle)
-      bullets.fireBullet(this.player?.body.x, this.player?.body.y, angle)
-    })
+    
   }
-
-  checkDoubleEligibility (
-    key: Phaser.Input.Keyboard.Key,
-    eligibilityState: Object
-  ) {
-    //Variable declarations
-    const { keyCode } = key
-    const lastTime = eligibilityState[keyCode]?.lastTime ?? 0
-    const currentTime = this.time.now
-    const isJustPressed = Phaser.Input.Keyboard.JustDown(key)
-    const deltaTime = currentTime - lastTime
-    let canDouble = eligibilityState[keyCode]?.canDouble ?? false
-    //Logic
-    isJustPressed && canDouble && deltaTime < 200
-      ? (canDouble = true)
-      : (canDouble = false)
-    if (canDouble)
-      console.table({ hayai: '早い', deltaTime, currentTime, lastTime })
-    //State update
-    eligibilityState[keyCode] = {
-      canDouble: !canDouble, // Can't double if doubled
-      lastTime: currentTime
-    }
-    // Return
-    return canDouble
-  }
-
   update (t: number, dt: number) {
-    if (!this.player) return
-    // Esquerda
-    const sideDash = 8600
-    const sideRun = 160
-    if (!this.cursors?.right?.isDown && this.cursors?.left?.isDown) {
-      if (
-        this.checkDoubleEligibility(
-          this.cursors.left as Phaser.Input.Keyboard.Key,
-          this.doublePressEligibility
-        )
-      ) {
-        this.player?.setVelocityX(-sideDash)
-      } else {
-        this.player?.setVelocityX(-sideRun)
-      }
-      this.player?.anims.play('left', true)
-    } else if (this.cursors?.right?.isDown && !this.cursors?.left?.isDown) {
-      if (
-        this.checkDoubleEligibility(
-          this.cursors.right as Phaser.Input.Keyboard.Key,
-          this.doublePressEligibility
-        )
-      ) {
-        this.player?.setVelocityX(sideDash)
-      } else {
-        this.player?.setVelocityX(sideRun)
-      }
-      this.player?.anims.play('right', true)
-    } else {
-      this.player?.setVelocityX(0)
-      this.player?.anims.play('turn')
-    }
 
-    if (this.cursors?.up?.isDown && this.player?.body.touching.down) {
-      this.player.setVelocityY(-330)
-    }
+    this.player.update(t, dt, this.cursors)
+    if (!this.player) return
   }
 }
